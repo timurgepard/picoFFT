88fe14d20c15e9868b42339d42e8b778a339bcf2
timurgepard <timur-ishuov@rambler.ru>
2024-11-18 16:50:49 +0100

    Update fft.py

--- fft.py
+++ fft.py
@@ -1,181 +1,166 @@
 import torch
+import torch.nn as nn
+import torch.nn.functional as F
 import random
-from fft import BigramLanguageModel
-import pickle
-import re
+import torch.jit as jit
+import math
+
+
+
 
 torch.manual_seed(1337)
-scaler = torch.amp.GradScaler('cuda')
-
-batch_size = 64
-time_intervals = 384
-max_iter = 1000000
-eval_interval = 250
-learning_rate = 3e-5
-eval_iters = 10
-
-vocab_embed = 768
-n_embed = 768
-layers = 17
-facets = 4
 
 
+def tril_init(linear):
+    with torch.no_grad():
+        linear.weight.copy_(torch.tril(linear.weight))
+
+# Zero out gradients
+def get_zero_grad_hook(mask):
+    def hook(grad):
+        return grad * mask
+    return hook
 
 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-print(device)
+#ReSine Activation Function
+# nn.Module -> JIT C++ graph
+class ReSine(jit.ScriptModule):
+    def __init__(self, n_embed=256):
+        super(ReSine, self).__init__()
 
-
-with open('./input/tokens.pkl', 'rb') as f:
-    tokens  = pickle.load(f)
-
-with open('./input/input.pkl', 'rb') as f:
-    input_tokens  = pickle.load(f)
-
-vocab_size = len(tokens)
-
-
-print(tokens)
-print('vocab token size: ', vocab_size)
-print('input text token size: ', len(input_tokens))
-
-
-stoi = {ch:i for i,ch in enumerate(tokens)}
-itos = {i:ch for i,ch in enumerate(tokens)}
-
-encode = lambda s: [stoi[c] for c in s]
-decode = lambda l: ''.join([itos[i] for i in l])
+        self.s = (2*torch.rand(n_embed)-1).to('cuda')
+        self.r = (10*torch.rand(n_embed)-5).to('cuda')
 
 
 
+        self.ffw1 = nn.Linear(n_embed, n_embed, bias=True)
+        self.ffw2 = nn.Linear(n_embed, n_embed, bias=True)
 
-data = torch.tensor(encode(input_tokens), dtype=torch.long)
+        # manual bias initialization with Glorot Uniform
+        std = 1.0/math.sqrt(n_embed)
+        noise = 2*std*torch.rand(n_embed)-std
+        self.ffw1.bias = nn.Parameter(data=noise, requires_grad=True)
+        std = 1.0/math.sqrt(n_embed)
+        noise = 2*std*torch.rand(n_embed)-std
+        self.ffw2.bias = nn.Parameter(data=noise, requires_grad=True)
+        
 
-
-torch.manual_seed(135665)
-
-
-def get_batch():
-    #var_time = time_intervals - 2*random.randint(0, 2)
-    var_time = time_intervals
-    ix = torch.randint(len(data) - var_time, (batch_size, ))
-    x = torch.stack([data[i:i+var_time] for i in ix])
-    y = torch.stack([data[i+1:i+var_time+1] for i in ix])
-    return x.to(device), y.to(device)
-
-
-def get_random_block():
-    #var_time = time_intervals - 2*random.randint(0, 50)
-    var_time = time_intervals
-    i = random.randint(0, len(data) - var_time)
-    block = data[i:i+var_time].reshape(1, -1).to(device)
-    return block
-
-
-@torch.no_grad()
-def estimate_loss():
-    LLM.eval()
-    losses = torch.zeros(eval_iters)
-    for k in range(eval_iters):
-        X, Y = get_batch()
-        _, _, loss = LLM.update(X, targets=Y)
-        losses[k] = loss.item()
-    out = losses.mean()
-    LLM.train()
-    return out
-
-def text_correct(text, multiline=True):
-    def cap(match):
-        return(match.group().capitalize())
-    p = re.compile(r'(?<=[\.\?!][\s\n])(\w+)')
-    text = text.replace(" i ", " I ")
-    text = text.replace(" lord ", " Lord ")
-    text = text.replace(" god ", " God ")
-    for _ in range(7):
-        text = text.replace(" : ", ": ")
-        text = text.replace(" ! ", "! ")
-        text = text.replace(" ? ", "? ")
-        text = text.replace(" . ", ". ")
-        text = text.replace(" , ", ", ")
-    if multiline: text = text.replace(" * ", "\n")
-    for _ in range(7): text = text.replace("  ", " ")
-
-
-    text = p.sub(cap, text)
-    return text
-
-
-LLM = BigramLanguageModel(vocab_size, time_intervals, vocab_embed=vocab_embed, n_embed=n_embed, facets=facets, n_layers=layers, device=device).to(device)
-optimizer = torch.optim.AdamW(LLM.parameters(), lr=learning_rate)
-
-pytorch_total_params = sum(p.numel() for p in LLM.parameters())
-print('LLM parameters: ', pytorch_total_params)
-
-
-try:
-    LLM.load_state_dict(torch.load('LLM_model.pt',  weights_only=True))
-    context = get_random_block()
-    text = decode(LLM.generate(context, max_new_tokens=1000)[0].tolist())[-1000:]
-    print(text_correct(text))
-
-    print("loaded")
-except:
-    print("no LLM")
-
-for iter in range(max_iter):
-
-
-    if iter % eval_interval == 0:
-        losses = estimate_loss()
-        context = get_random_block()
-        text = decode(LLM.generate(context, max_new_tokens=100)[0].tolist())[-100:]
-        text = text_correct(text, multiline=False)
-
-        print(f"step {iter}, train loss: {losses:.4f}, text: {text}")
-        if iter>=500:
-            try:
-                torch.save(LLM.state_dict(), 'LLM_model.pt')
-            except:
-                print("problem during saving LLM")
-
-        if iter>=10000 and iter%10000==0:
-            context = get_random_block()
-            text = decode(context[0].tolist())
-            print(text_correct(text))
-            print("###########################################")
-            print("###########################################")
-            text = decode(LLM.generate(context, max_new_tokens=500)[0].tolist())
-            print(text_correct(text))
-            print("###########################################")
-            print("###########################################")
-
-    #sample batch of data
-    xb, yb = get_batch()
-
-    #evaluate the loss
-    with torch.amp.autocast('cuda', dtype=torch.float16):
-        _, _, loss = LLM.update(xb, targets=yb)
-    optimizer.zero_grad(set_to_none=True)
-    scaler.scale(loss).backward()
-    scaler.step(optimizer)
-    scaler.update()
-
-
-#generate from the LLM
-#context = torch.ones((1,1), dtype=torch.long, device=device)
-
-context = get_random_block()
-
-text = decode(context[0].tolist())
-print(text_correct(text))
-
-print("###########################################")
-print("###########################################")
-print("###########################################")
-
-
-text = decode(LLM.generate(context, max_new_tokens=500)[0].tolist())
-print(text_correct(text))
+    @jit.script_method
+    def forward(self, x):
+        x = self.ffw1(x)
+        x = self.s*torch.sin(x/self.s)
+        x = self.ffw2(x)
+        return x * torch.sigmoid(self.r*x) 
 
 
 
+class Block(jit.ScriptModule):
+    def __init__(self, time_intervals, n_embed, facets, tri_W):
+        super().__init__()
+
+        self.f = facets
+        self.E_f = n_embed//facets
+
+        self.fft= nn.Sequential(
+            nn.Linear(time_intervals, time_intervals, bias=None),
+            nn.Linear(time_intervals, time_intervals, bias=None)
+        )
+
+        self.fft[0].apply(tril_init)
+        self.fft[0].weight.register_hook(get_zero_grad_hook(tri_W))
+        self.fft[1].apply(tril_init)
+        self.fft[1].weight.register_hook(get_zero_grad_hook(tri_W))
+
+      
+        self.ffw = ReSine(n_embed)
+
+        self.ln1 = nn.LayerNorm(self.E_f)
+        self.ln2 = nn.LayerNorm(self.E_f)
+
+    @jit.script_method
+    def forward(self, x):
+        B, T, E = x.shape
+        x = self.ln1(x.reshape(B, T, self.f, self.E_f))
+        x += self.fft(x.reshape(B, self.f, self.E_f, T)).reshape(B, T, self.f, self.E_f)
+        x = self.ln2(x).reshape(B, T, E)
+        return x + self.ffw(x)
+
+
+
+class BigramLanguageModel(jit.ScriptModule):
+    def __init__(self, vocab_size, time_intervals, vocab_embed, n_embed, facets, n_layers, device="cpu"):
+        super().__init__()
+        self.device = device
+        self.tok_emb = nn.Embedding(vocab_size, vocab_embed)
+        self.pos_emb = nn.Embedding(time_intervals, vocab_embed)
+
+        self.seq = torch.arange(time_intervals, device=device)
+
+        self.ln_in = nn.LayerNorm(vocab_embed)
+        self.uniform = nn.Linear(vocab_embed, n_embed)
+
+
+        tri = torch.tril(torch.ones((time_intervals, time_intervals), dtype=torch.float32)).to(device)
+        tri_W = tri/tri.sum(dim=1, keepdim=True)
+
+        self.blocks = nn.Sequential(*[Block(time_intervals, n_embed, facets, tri_W.detach()) for _ in range(n_layers)])
+        
+        self.ln_out = nn.LayerNorm(n_embed)
+
+        self.linear_head = nn.Linear(n_embed, vocab_size)
+
+        self.time_intervals = time_intervals
+
+        self.cdist = torch.distributions.categorical
+
+
+
+    @jit.script_method
+    def forward(self, idx):
+
+
+        x = self.uniform(self.ln_in(self.tok_emb(idx) + self.pos_emb(self.seq)))
+
+        embed  = self.ln_out(self.blocks(x))
+
+        logits = self.linear_head(embed)
+
+        return embed, logits
+
+    @jit.script_method
+    def update(self, idx, targets):
+        embed, logits = self(idx)
+        B, T, V = logits.shape
+        logits = logits.view(B*T, V)
+        targets = targets.view(B*T)
+        loss = F.cross_entropy(logits, targets)
+        return embed, logits, loss
+
+
+    
+    def decode(self, idx):
+        with torch.no_grad():
+            _, logits = self(idx)
+            probs = F.softmax(logits, dim=-1)
+            m = self.cdist.Categorical(probs)
+            idx = m.sample()
+            return idx
+
+    def generate(self, idx, max_new_tokens, LLM=None):
+        #idx is (B, T) array of indices in the current context
+        with torch.no_grad():
+            for _ in range(max_new_tokens):
+                # crop idx to the last block_size tokens
+                idx_cond = idx[:, -self.time_intervals:]
+                # get the predictions
+                idx_cond_next = LLM.decode(idx_cond) if LLM != None else idx_cond
+                _, logits = self(idx_cond_next)
+                #focus only on the last time step
+                logits = logits[:, -1, :] #become (B, C)
+                # apply softmax to get probabilities
+                probs = F.softmax(logits, dim=-1) #(B, C)
+                # sample from distribution
+                idx_next = torch.multinomial(probs, num_samples=1) #(B,1)
+                # append sampled index to the running sequence
+                idx = torch.cat((idx, idx_next), dim=1) # (B, T+1)
+        return idx
